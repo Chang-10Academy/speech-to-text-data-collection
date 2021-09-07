07add368827593aaf4ccd905d33b38dc1682affe
smegnshd <smegnshdem@gmail.com>
2021-09-07 12:01:42 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirement_file:
+    requirements_list = requirement_file.readlines()
+    requirements_list = [lib.replace('\n', '') for lib in requirements_list]
+
+requirements = requirements_list
+
+test_requirements = []
+
+setup(
+    author="Ethani Caphace,Stella Kibika,Smegnsh Demelash, Boris Papineau, Binam Sisay,Germain Rukundo",
+    author_email="smegnshdem@gmail.com, ",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description=" Data collection for Speech to text.",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Chang-10Academy/speech-to-text-data-collection',
+    version='0.1.0',
+    zip_safe=False,
+)
