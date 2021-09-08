893edf8a767cc4044ca1a9242074a918e83e4b39
Caphace-Ethan <ethanyoung135@gmail.com>
2021-09-08 20:42:38 +0300

    Loading latest data values

--- front-end-service/producer.py
+++ front-end-service/producer.py
@@ -8,12 +8,12 @@ def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
 
-producer = KafkaProducer(bootstrap_servers=['172.21.0.3:9092'],
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=json_serializer)
 
 if __name__ == "__main__":
     while 1 == 1:
         registered_user = get_registered_user()
         print(registered_user)
-        producer.send("registered_user", registered_user)
+        producer.send("topic0001", registered_user)
         time.sleep(2)
