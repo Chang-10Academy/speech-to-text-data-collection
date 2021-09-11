375d29466058d0d1b778e9e595f208e8b67d316e
Yosef Alemneh <yosefworku67@gmail.com>
2021-09-11 19:00:43 +0300

    added web app

---
+++ scripts/consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'audiostore',
+     bootstrap_servers=['localhost:9092'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8'))
+     )
+
+for message in consumer:
+    message = message.value
+    print('{} consumed'.format(message))
