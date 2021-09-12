c879308068c0fa7149777c4c3c968ec5577aa26b
Yosef Alemneh <yosefworku67@gmail.com>
2021-09-13 01:55:20 +0300

    consumer saves audio and transscript as .wav and txt files

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -1,15 +1,29 @@
 from kafka import KafkaConsumer
 from json import loads
+import librosa
+import random
+import numpy as np
+import logging
 
 consumer = KafkaConsumer(
     'audiostore',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     enable_auto_commit=True,
-     group_id='my-group',
-     value_deserializer=lambda x: loads(x.decode('utf-8'))
-     )
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group',
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+
+
 
 for message in consumer:
     message = message.value
-    print('{} consumed'.format(message))
+    print('consumed: {} {} {}'.format(message['transcript'], message['sample_rate'], message['audio'][:10]))
+    transcripts = open('../data/Transcripts.txt', 'a', encoding = "utf-8")
+
+    filename = str(random.randint(1,10**12))
+    transcripts.write("({}) : {}\n".format(filename, message['transcript']))
+    transcripts.close()
+    librosa.output.write_wav('../data/audio/{}.wav'.format(filename), np.array(message['audio']), message['sample_rate'])
+    logging.info('consumed: {} {} {}'.format(message['transcript'], message['sample_rate'], message['audio'][:10]))
+    
--- scripts/producer.py
+++ scripts/producer.py
@@ -1,12 +1,17 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import random
+
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
-
+text_coll = open('../data/texts/data_3.txt', 'r', encoding="utf8")
+texts = list(map(lambda x: x.strip(), text_coll.readlines()))
 for e in range(10):
-    data = {'text': str(e), 'audio' : [e]}
+    data = {"transcript": texts[random.randint(0, len(texts)-1)], "sample_rate": 8000, "audio": [0]*10}
+    print("produced:", data)
     producer.send('audiostore', value=data)
     sleep(5)
+text_coll.close()
