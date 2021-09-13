bed7d0a331da0fc0ed4e3962ef5ba747e43fa2bf
Stella Kibika <steshykibika@gmail.com>
2021-09-13 14:22:11 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,14 +11,6 @@ from kafka.admin import KafkaAdminClient,NewTopic
 
 app = Flask(__name__)
 
-# admin_client = KafkaAdminClient(bootstrap_servers=["b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092",
-# "b-2.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092"],
-# client_id='d_id')
-
-# topic_list = []
-# topic_list.append(NewTopic(name="audio", num_partitions=1, replication_factor=2))
-# admin_client.create_topics(new_topics=topic_list, validate_only=False)
-
 
 TOPIC_NAME = 'audio'
 
