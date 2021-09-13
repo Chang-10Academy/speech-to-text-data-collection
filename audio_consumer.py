c990611048e581af090821a9ae0ef9bdec95de51
Stella Kibika <steshykibika@gmail.com>
2021-09-13 20:50:11 +0300

    Update audio_consumer.py

--- audio_consumer.py
+++ audio_consumer.py
@@ -7,8 +7,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 if __name__ == "__main__":
 	TOPIC_NAME = "audio"
- 	consumer = KafkaConsumer('TOPIC_NAME',client_id='d_id',bootstrap_servers=["b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092","b-2.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092"],auto_offset_reset='earliest', enable_auto_commit=True)
-    	for event in consumer:
+ 	consumer = KafkaConsumer('TOPIC_NAME', client_id='d_id', bootstrap_servers=["b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092", "b-2.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092"], auto_offset_reset='earliest', enable_auto_commit=True)
+	for event in consumer:
 		event_data = event.value
 		print(event_data)
 	
