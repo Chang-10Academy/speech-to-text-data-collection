5edd16fda56d711353ad3dc02e61aabf0e825a40
Stella Kibika <steshykibika@gmail.com>
2021-09-13 20:40:04 +0300

    Update audio_consumer.py

--- audio_consumer.py
+++ audio_consumer.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     KAFKA_SERVER = "localhost:29092"
 
  
-    consumer = KafkaConsumer(
-        TOPIC_NAME,
-        # to deserialize kafka.producer.object into dict
-        value_deserializer=lambda m: json.loads(m.decode('utf-8')),
-    )
-    for audio in consumer:
-        print("Audio = {}".format(json.loads(audio.value)))
+    consumer = KafkaConsumer('TOPIC_NAME',client_id='d_id',bootstrap_servers=["b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092",
+"b-2.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092"],auto_offset_reset='earliest',
+                             enable_auto_commit=True)
+    
+    for event in consumer:
+	event_data=event.value
+        print(event_data)
 	
