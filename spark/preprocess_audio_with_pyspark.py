9340dac17275f530372ad07ea9b119c6933c18ca
Yosef Alemneh <yosefworku67@gmail.com>
2021-09-15 01:05:12 +0300

    modified pyspark code

--- spark/preprocess_audio_with_pyspark.py
+++ spark/preprocess_audio_with_pyspark.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from pyspark import SparkContext, SparkConf
 import librosa
 
@@ -7,9 +8,12 @@ sc = SparkContext.getOrCreate(conf)
 
 def preprocess(path):
     # resample at 8000 Hz
-    sampled_audio = librosa.load('../data/audio/' + path, sr = 8000)
-    librosa.output.write_wav('../data/preprocessed_audio/' + path, sampled_audio, 8000) 
+    sampled_audio, sr = librosa.load('../data/audio/' + path, sr = 8000)
+    librosa.output.write_wav(path = '../data/preprocessed_audio/' + path, y = sampled_audio, sr = 8000) 
 
 def preprocess_audios():
     filepaths = os.listdir('../data/audio/')
     rdd = sc.parallelize(filepaths, len(filepaths)).foreach(preprocess)
+if __name__ == '__main__':
+    preprocess_audios()
+
