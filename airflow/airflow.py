d3f2acada1229c5b19026bd3d52d645ecab96ed5
Stella Kibika <steshykibika@gmail.com>
2021-09-14 17:01:58 +0300

    Update airflow.py

--- airflow/airflow.py
+++ airflow/airflow.py
@@ -40,36 +40,36 @@ def save_processed_audio_to_s3():
 
 with DAG( catchup=False, dag_id='audio_processing_dag', schedule_interval='*/1 * * * *', description='Amharic speech data audio processing dag', default_args=default_args) as dag:
   
-  read_unprocessed_data_from_consumer = PythonOperator(
-    task_id='read_unprocessed_data_from_s3', 
-    python_callable=read_unprocessed_data_from_s3, 
-    dag=dag)
+  read_unprocessed_data_from_producer = PythonOperator(
+    task_id='read_unprocessed_data_from_app',
+    python_callable=generate_stream,              # function to be executed
+    dag=dag,
+)
+#   spark_audio_processing = PythonOperator(
+#     task_id='spark_audio_processing', 
+#     python_callable=spark_audio_processing, 
+#     dag=dag)
 
-  spark_audio_processing = PythonOperator(
-    task_id='spark_audio_processing', 
-    python_callable=spark_audio_processing, 
-    dag=dag)
+#   # spark_audio_processing = PythonOperator(
+#   #   task_id='spark_audio_processing', 
+#   #   python_callable=spark_audio_processing, 
+#   #   op_kwargs = {'config' : config},
+#   #   provide_context=True,
+#   #   dag=dag
+#   # )
 
-  # spark_audio_processing = PythonOperator(
-  #   task_id='spark_audio_processing', 
-  #   python_callable=spark_audio_processing, 
-  #   op_kwargs = {'config' : config},
-  #   provide_context=True,
-  #   dag=dag
-  # )
+#   save_processed_audio_to_s3 = PythonOperator(
+#     task_id='save_processed_audio_to_s3', 
+#     python_callable=save_processed_audio_to_s3, 
+#     dag=dag)
 
-  save_processed_audio_to_s3 = PythonOperator(
-    task_id='save_processed_audio_to_s3', 
-    python_callable=save_processed_audio_to_s3, 
-    dag=dag)
+#   # spark_job = BashOperator(
+#   #   task_id='spark_task_etl',
+#   #   bash_command='spark-submit --master spark://localhost:7077 spark_job.py',
+#   #   dag = dag)
 
-  # spark_job = BashOperator(
-  #   task_id='spark_task_etl',
-  #   bash_command='spark-submit --master spark://localhost:7077 spark_job.py',
-  #   dag = dag)
-
-  # setting dependencies
+#   # setting dependencies
 
 
-  read_unprocessed_data_from_s3 >> spark_audio_processing
-  spark_audio_processing >> save_processed_audio_to_s3
+#   read_unprocessed_data_from_s3 >> spark_audio_processing
+#   spark_audio_processing >> save_processed_audio_to_s3
